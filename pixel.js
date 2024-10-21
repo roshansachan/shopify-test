70106e71bec10184f38f5598770ca2e6da169f09
roshan <roshanjsachan@gmail.com>
2024-10-21 09:39:16 +0530

    Update pixel.js

--- pixel.js
+++ pixel.js
@@ -12,7 +12,7 @@
     const date = new Date();
     date.setTime(date.getTime() + 2592000000); // 30 days in milliseconds
     const expires = "; expires=" + date.toUTCString();
-    const domain = "; domain=.ondigitalocean.app; path=/;Secure; SameSite=None"
+    const domain = "; domain=ondigitalocean.app; path=/;Secure; SameSite=None"
     document.cookie = name + "=" + (value || "") + expires + domain + ";";
   };
 
